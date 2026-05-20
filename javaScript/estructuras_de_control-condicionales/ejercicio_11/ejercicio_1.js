ba8cfc5100aef321ddbb2717805987c8635fd3f1
Jonas Campos <jonas.campos.programacion@hotmail.com>
2026-05-20 20:39:47 +0200

    Ejercicios 11 y 12  de estructuras de control terminados

--- javaScript/estructuras_de_control-condicionales/ejercicio_11/ejercicio_1.js
+++ javaScript/estructuras_de_control-condicionales/ejercicio_11/ejercicio_1.js
@@ -7,3 +7,73 @@ Crear un programa que permita elegir entre las operaciones: SUMA, RESTA, DIVISIO
 y luego de elegida, ingresar dos números y hacer dicha operación con ella.
 
 */
+
+let operacion ;
+
+do{
+    operacion= prompt(`Elige que operación quieres realizar: suma, resta, multiplicación o división. `).toLowerCase().trim();
+
+    if( operacion!==`suma` &&
+        operacion!==`resta` &&
+        operacion!==`multiplicacion` &&
+        operacion!==`division` 
+    ){
+        window.alert(`Elige correctamente.`);
+        operacion=`error`;
+    }
+
+}while(operacion ===`error`);
+
+let numero1;
+    do{
+    numero1=parseFloat(prompt(`Escribe el primer número: `));
+    if (isNaN(numero1)){
+        window.alert(`Esto no es un número, por favor ingrese un número`);
+    }
+
+    }while(isNaN(numero1))
+
+    let numero2;
+    do{
+    numero2=parseFloat(prompt(`Escribe el segundo número: `));
+    if (isNaN(numero2)){
+        window.alert(`Esto no es un número, por favor ingrese un número`);
+    }
+
+    }while(isNaN(numero2))
+
+window.alert(`Vamos a realizar la operación de ${operacion} de estos dos números ${numero1} y ${numero2}.`)   
+
+let resultado;
+
+switch(operacion){
+    case `suma`:
+    resultado = numero1+numero2;
+    window.alert(`El resultado de la suma es de: ${resultado}`);
+    break;
+    
+    case `resta`:
+        resultado = numero1-numero2;
+        window.alert(`El resultado de resta es de: ${resultado}`);
+        break;
+
+    case `multiplicacion`:
+   
+        resultado = numero1*numero2;
+        window.alert(`El resultado de la multiplicación es de: ${resultado}`);
+        break;
+        
+    case `division`:
+        do{
+            if (numero2 ===0){
+                window.alert(`El 0 no se puede dividir, por favor introduzca un número distinto a 0. `);
+                numero2=parseFloat(prompt(`Escriba otro número que sea distinto a 0:`));
+            }else if(isNaN(numero2)){
+                window.alert(`Escribe un número por favor.`);
+                numero2=parseFloat(prompt(`Escriba un número distinto a 0 y no pongas ninguna letra.....`));
+            }
+        }while(isNaN(numero2)|| numero2 ===0);
+    resultado = numero1/numero2;
+    window.alert(`El resultado de la división es de: ${resultado}`);
+    break;
+}
--- javaScript/estructuras_de_control-condicionales/ejercicio_12/ejercicio_1.js
+++ javaScript/estructuras_de_control-condicionales/ejercicio_12/ejercicio_1.js
@@ -19,3 +19,124 @@ Una vez elegida la opción e ingresado el valor si corresponde,
 debe mostrar un mensaje con el canal y el nivel del volumen actual.
 
 */
+
+let respuesta ;
+
+do{
+    respuesta=prompt(`¿Quieres encender la television? Si o no: `).toLowerCase().trim();
+
+    if ( respuesta === `no`){
+        window.alert(`Adios buenas tardes`)
+    }else if ( respuesta === `si`){
+        window.alert(`Genial, a continuacion te esplicamos las opciones que tienes.`);
+    }else{
+        window.alert(`No entendí tu respuesta.`)
+    }
+}while(respuesta !== `no`&& respuesta !== `si`);
+
+let canal = 23;
+let volumen = 10;
+
+while(respuesta === `si`){
+    
+    window.alert(`Aqui le paso las instrucciones a seguir:
+        CAMBIAR CANAL: permite ingresar un canal (0 al 99)
+        CANAL SIGUIENTE: sube un canal
+        CANAL ANTERIOR: baja un canal
+        SUBIR VOLUMEN: sube en volumen en 5
+        BAJAR VOLUMEN: disminuye en volumen en 5
+        MUTEAR: pone el volumen en 0
+        APAGAR : apaga el televisor.
+        `);
+
+
+    let opcion;
+
+    
+    do{
+        opcion= prompt(`Elige la opcion deseada`).toUpperCase().trim();
+
+        if(opcion !== `CAMBIAR CANAL`&&
+            opcion !== `CANAL SIGUIENTE`&&
+            opcion !== `CANAL ANTERIOR`&&
+            opcion !== `SUBIR VOLUMEN`&&
+            opcion !== `BAJAR VOLUMEN`&&
+            opcion !== `MUTEAR`&&
+            opcion !== `APAGAR`)
+            {
+                opcion = `error`
+                window.alert(`No te entiendo`);
+            }
+    }while(opcion===`error`);
+
+    
+    
+    
+    switch (opcion){
+        
+
+
+        case `CAMBIAR CANAL`:
+           let canalUsuario;
+            do{
+                canalUsuario = parseInt(prompt(`Introduce el número del nuevo canal:`));
+                if( canalUsuario >=0 && canalUsuario<=99){
+                    canal = canalUsuario;
+                    window.alert(`El canal ahora es: ${canalUsuario}`);
+                }else{
+                    window.alert(`Introduce un canal valido`);
+                    canalUsuario=`error`;
+                }
+            }while(canalUsuario === `error`);
+            break;
+        case `CANAL SIGUIENTE`:
+            canal+=1
+            if ( canal > 99){
+                window.alert(`No se puede realizar esta opcion`);
+                canal-=1
+
+            }else if ( canal <= 99){
+                
+                window.alert(`Cambio de canal realizado con exito, tu canal actual es: ${canal}.`);
+            }
+           break;
+        case `CANAL ANTERIOR`:
+            canal -=1
+            if ( canal < 0){
+                window.alert(`No se puede realizar esta opcion`);
+                canal+=1
+            }else if ( canal >= 0){
+                
+                window.alert(`Cambio de canal realizado con exito, tu canal actual es: ${canal}.`);
+            }
+           break;
+        case `SUBIR VOLUMEN`:
+            volumen+=5;
+            window.alert(`El volumen actual es de: ${volumen} puntos.`);
+           
+            break;
+        case `BAJAR VOLUMEN`:
+            volumen-=5;
+            if(volumen < 0){
+                window.alert(`Menos que nada no se puede escuchar ajjajajaja.`);
+                volumen +=5;
+            }else {
+                
+                window.alert(`El volumen actual es de: ${volumen} puntos.`);
+            }
+            break;
+        case `MUTEAR`:
+            volumen = 0;
+            window.alert(`MUTE`);
+            break;
+
+        case `APAGAR`:
+            respuesta = `no`;
+            window.alert(`Chau pescau.`);
+            break;
+    
+    }
+    
+
+    window.alert(`La tele se encuentra en el canal ${canal} y su volumen es de ${volumen}`)
+}
